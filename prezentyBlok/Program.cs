bd522c427e78b3f8652dcd7a0e63b159cf11033c
5m <konradek982@gmail.com>
2024-12-03 10:55:16 +0100

    Dodaj pliki projektów.

---
+++ prezentyBlok/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace prezentyBlok
+{
+    internal class Program
+    {
+        static private int zliczaniePietr(string plusyMinusy)
+        {
+            int pietro = 0;
+            foreach(char c in plusyMinusy)
+            {
+                if (c == '+')
+                    pietro++;
+                else if (c == '-')
+                    pietro--;
+            }
+
+            return pietro;
+        }
+        static void Main(string[] args)
+        {
+            int.TryParse(Console.ReadLine(), out int n);
+            List<int> wyniki = new List<int>();
+
+            for (int i = 0; i < n; i++) {
+                wyniki.Add(zliczaniePietr(Console.ReadLine()));
+            }
+
+            for(int i = 0; i < n; i++) {
+                Console.WriteLine(wyniki[i]);
+            }
+
+        }
+    }
+}
